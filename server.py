71271e84148d3fa8cd2baefc33afe0e686938a79
harshithharshu <harshithharshu9480@gmail.com>
2018-10-14 23:39:26 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify
+from pyhoroscope import Horoscope
+
+app = Flask (__name__)
+
+############################################
+# Index 
+############################################
+
+@app.route ('/', methods=['GET'])
+def index_route () :
+    return jsonify({
+		'author' : 'Harshith',
+		'base_url' : 'https://astroapiproject.herokuapp.com/',
+	    	'project_name' : 'Horoscope API',
+		'project_url' : 'https://astroapiproject.herokuapp.com/'
+	})
+
+
+############################################
+# Horoscopes
+###########################################
+
+#Todays' Horoscope
+@app.route ('/horoscope/today/<sunsign>', methods=['GET'])
+def today_horoscope_route (sunsign) :
+	result = dict (Horoscope.get_todays_horoscope (sunsign))
+	return jsonify (date=result['date'],
+			sunsign=result['sunsign'],
+			horoscope=result['horoscope'])
+					
+
+#Current Week Horoscope
+@app.route ('/horoscope/week/<sunsign>', methods=['GET'])
+def weekly_horoscope_route (sunsign) :
+	result = dict (Horoscope.get_weekly_horoscope (sunsign))
+	return jsonify (week=result['week'],
+			sunsign=result['sunsign'],
+			horoscope=result['horoscope'])
+
+#Current Month Horoscope
+@app.route ('/horoscope/month/<sunsign>', methods=['GET'])
+def monthly_horoscope_route (sunsign) :
+	result = dict (Horoscope.get_monthly_horoscope (sunsign))
+	return jsonify (month=result['month'],
+			sunsign=result['sunsign'],
+			horoscope=result['horoscope'])
+
+#Current Year Horoscope
+@app.route ('/horoscope/year/<sunsign>', methods=['GET'])
+def yearly_horoscope_route (sunsign) :
+	result = dict (Horoscope.get_yearly_horoscope (sunsign))
+	return jsonify (year=result['year'],
+			sunsign=result['sunsign'],
+			horoscope=result['horoscope'])
+
+
+###########################################
+#Start Flask
+###########################################
+
+if __name__ == "__main__":
+	app.run()
