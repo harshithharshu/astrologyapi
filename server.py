8ee1cbcdbcf8d2326814ab7d673cde7d12612991
harshithharshu <harshithharshu9480@gmail.com>
2018-10-15 00:42:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 from pyhoroscope import Horoscope
-
+from flask_cors import CORS
 app = Flask (__name__)
-
+CORS(app)
 ############################################
 # Index 
 ############################################
